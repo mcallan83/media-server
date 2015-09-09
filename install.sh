c2a2811c716523e827ffedad69663f09ee9c8a40
Mike Callan <mcallan83@gmail.com>
2015-09-08 22:58:56 -0500

    readme

--- install.sh
+++ install.sh
@@ -6,54 +6,54 @@ sudo -v
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# # Install Avahi
-# sudo apt-get install avahi-daemon -y
-# sudo service avahi-daemon restart
+# Install Avahi
+sudo apt-get install avahi-daemon -y
+sudo service avahi-daemon restart
 
-# # Install Webmin
-# echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
-# echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
-# wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+# Install Webmin
+echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
+echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
+wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
 
-# sudo apt-get update -y
-# sudo apt-get install webmin -y
-# sudo sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-# sudo /etc/init.d/webmin restart
+sudo apt-get update -y
+sudo apt-get install webmin -y
+sudo sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+sudo /etc/init.d/webmin restart
 
-# # Install Sonarr
-# sudo add-apt-repository -y ppa:ermshiperete/monodevelop
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
-# echo "deb http://update.nzbdrone.com/repos/apt/debian master main" | sudo tee -a /etc/apt/sources.list
+# Install Sonarr
+sudo add-apt-repository -y ppa:ermshiperete/monodevelop
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
+echo "deb http://update.nzbdrone.com/repos/apt/debian master main" | sudo tee -a /etc/apt/sources.list
 
-# sudo apt-get update -y
-# sudo apt-get -y install mono-complete nzbdrone
+sudo apt-get update -y
+sudo apt-get -y install mono-complete nzbdrone
 
-# sudo cp templates/sonarr/sonarr-init /etc/init.d/nzbdrone
-# sudo chown $UNAME: /etc/init.d/nzbdrone
-# sudo chmod +x /etc/init.d/nzbdrone
-# if [ ! -d "/var/run/nzbdrone" ]; then
-# 	sudo mkdir /var/run/nzbdrone
-# fi
-# sudo sed -i 's/RUN_AS=USERNAME/RUN_AS='$UNAME'/g' /etc/init.d/nzbdrone
-# sudo chmod +x /etc/init.d/nzbdrone
-# sudo update-rc.d nzbdrone defaults
-# sudo service nzbdrone start >/dev/null 2>&1
+sudo cp templates/sonarr/sonarr-init /etc/init.d/nzbdrone
+sudo chown $UNAME: /etc/init.d/nzbdrone
+sudo chmod +x /etc/init.d/nzbdrone
+if [ ! -d "/var/run/nzbdrone" ]; then
+	sudo mkdir /var/run/nzbdrone
+fi
+sudo sed -i 's/RUN_AS=USERNAME/RUN_AS='$UNAME'/g' /etc/init.d/nzbdrone
+sudo chmod +x /etc/init.d/nzbdrone
+sudo update-rc.d nzbdrone defaults
+sudo service nzbdrone start >/dev/null 2>&1
 
-# # Install Deluge
-# sudo add-apt-repository -y ppa:deluge-team/ppa
-# sudo apt-get update -y
-# sudo apt-get -y install deluged deluge-webui
+# Install Deluge
+sudo add-apt-repository -y ppa:deluge-team/ppa
+sudo apt-get update -y
+sudo apt-get -y install deluged deluge-webui
 
-# sudo cp templates/deluge/deluge.conf /etc/init/deluge.conf
-# sudo sed -i 's/env uid=USER/env uid='$UNAME'/g' /etc/init/deluge.conf
-# sudo sed -i 's/env gid=GROUP/env gid='$UGROUP'/g' /etc/init/deluge.conf
+sudo cp templates/deluge/deluge.conf /etc/init/deluge.conf
+sudo sed -i 's/env uid=USER/env uid='$UNAME'/g' /etc/init/deluge.conf
+sudo sed -i 's/env gid=GROUP/env gid='$UGROUP'/g' /etc/init/deluge.conf
 
-# sudo cp templates/deluge/deluge-web.conf /etc/init/deluge-web.conf
-# sudo sed -i 's/env uid=USER/env uid='$UNAME'/g' /etc/init/deluge-web.conf
-# sudo sed -i 's/env gid=GROUP/env gid='$UGROUP'/g' /etc/init/deluge-web.conf
+sudo cp templates/deluge/deluge-web.conf /etc/init/deluge-web.conf
+sudo sed -i 's/env uid=USER/env uid='$UNAME'/g' /etc/init/deluge-web.conf
+sudo sed -i 's/env gid=GROUP/env gid='$UGROUP'/g' /etc/init/deluge-web.conf
 
-# sudo start deluge
-# sudo start deluge-web
+sudo start deluge
+sudo start deluge-web
 
 # Install Plex
 sudo apt-get install curl -y
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Media Server Config
+
+## Reference Sites
+
+- [http://www.cuttingcords.com/home/ultimate-server/getting-started](http://www.cuttingcords.com/home/ultimate-server/getting-started)
+- [http://www.htpcbeginner.com/](http://www.htpcbeginner.com/)
+
+## Ports
+
+- Deluge: 8112
+- Plex: 32400
+- Sonarr: 8989
+- Webmin: 10000
