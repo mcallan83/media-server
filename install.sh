d22cdd342e9c8080a53a312f21a97f84dc6f2440
Mike Callan <mcallan83@gmail.com>
2015-09-08 21:04:19 -0500

    install avahi

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ sudo -v
 
 sudo apt-get update
 sudo apt-get upgrade
+
+# Install Avahi
+sudo apt-get install avahi-daemon -y
+sudo cp ~/templates/avahi/ssh.service /etc/avahi/services/ssh.service
+sudo service avahi-daemon restart
