68bb5e765f798baac0458c2f2ffd67d1eced7868
Mike Callan <mcallan83@gmail.com>
2015-09-08 21:13:49 -0500

    install sonarr

--- install.sh
+++ install.sh
@@ -18,3 +18,21 @@ wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install webmin -y
 sudo sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+
+# Install Sonarr
+sudo add-apt-repository -y ppa:ermshiperete/monodevelop
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
+echo "deb http://update.nzbdrone.com/repos/apt/debian master main" | sudo tee -a /etc/apt/sources.list
+
+sudo apt-get update
+sudo apt-get -y install mono-complete nzbdrone
+
+sudo cp templates/sonarr/sonarr-init /etc/init.d/nzbdrone
+sudo chown $UNAME: /etc/init.d/nzbdrone
+sudo chmod +x /etc/init.d/nzbdrone
+if [ ! -d "/var/run/nzbdrone" ]; then
+	sudo mkdir /var/run/nzbdrone
+fi
+sudo sed -i 's/RUN_AS=USERNAME/RUN_AS='$UNAME'/g' /etc/init.d/nzbdrone
+sudo update-rc.d nzbdrone defaults
+sudo service nzbdrone start >/dev/null 2>&1
