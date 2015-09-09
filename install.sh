939cc702baed4e715bd64bd2ba2697b2c795b548
Mike Callan <mcallan83@gmail.com>
2015-09-08 21:53:08 -0500

    unattended installs

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ UGROUP=$(id -gn $UNAME)
 
 sudo -v
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # Install Avahi
 sudo apt-get install avahi-daemon -y
@@ -15,7 +15,7 @@ echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo t
 echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
 wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install webmin -y
 sudo sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 sudo /etc/init.d/webmin restart
@@ -25,7 +25,7 @@ sudo add-apt-repository -y ppa:ermshiperete/monodevelop
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
 echo "deb http://update.nzbdrone.com/repos/apt/debian master main" | sudo tee -a /etc/apt/sources.list
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install mono-complete nzbdrone
 
 sudo cp templates/sonarr/sonarr-init /etc/init.d/nzbdrone
@@ -40,7 +40,7 @@ sudo service nzbdrone start >/dev/null 2>&1
 
 # Install Deluge
 sudo add-apt-repository -y ppa:deluge-team/ppa
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install deluged deluge-webui
 
 sudo cp templates/deluge/deluge.conf /etc/init/deluge.conf
