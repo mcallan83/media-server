4c68be90c9c463645d5514bac0dafb4373af6b6c
Mike Callan <mcallan83@gmail.com>
2015-09-08 21:11:57 -0500

    install webmin

--- install.sh
+++ install.sh
@@ -8,5 +8,13 @@ sudo apt-get upgrade
 
 # Install Avahi
 sudo apt-get install avahi-daemon -y
-sudo cp ~/templates/avahi/ssh.service /etc/avahi/services/ssh.service
 sudo service avahi-daemon restart
+
+# Install Webmin
+echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
+echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
+wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install webmin -y
+sudo sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
