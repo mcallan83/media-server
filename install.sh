3c0c3bb9077234014c978e3eae4debf6c5ae5532
Mike Callan <mcallan83@gmail.com>
2015-09-14 21:33:03 -0500

    jackett

--- install.sh
+++ install.sh
@@ -14,19 +14,19 @@ sudo apt-get install avahi-daemon -y
 sudo service avahi-daemon restart
 
 # Install Webmin
-echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
-echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
-wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+# echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
+# echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
+# wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
 
-sudo apt-get update -y
-sudo apt-get install webmin -y
-sudo sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+# sudo apt-get update -y
+# sudo apt-get install webmin -y
+# sudo sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 
-# diabled until webmin works under nginx reverse proxy
-# echo "webprefix=/webmin" | sudo tee -a /etc/webmin/config
-# echo "webprefixnoredir=1" | sudo tee -a /etc/webmin/config
+# # diabled until webmin works under nginx reverse proxy
+# # echo "webprefix=/webmin" | sudo tee -a /etc/webmin/config
+# # echo "webprefixnoredir=1" | sudo tee -a /etc/webmin/config
 
-sudo /etc/init.d/webmin restart
+# sudo /etc/init.d/webmin restart
 
 # Install Sonarr
 sudo add-apt-repository -y ppa:ermshiperete/monodevelop
@@ -91,3 +91,17 @@ sudo ln -s /etc/nginx/sites-available/media media
 sudo service nginx restart
 
 sudo htpasswd -b -c /etc/nginx/htpasswd media $UPASS
+
+# Install Jackett
+wget http://jackett.net/Download/v0.6.4/Jackett.Mono.v0.6.4.tar.bz2
+tar -xvf Jackett.Mono.v0.6.4.tar.bz2
+sudo mkdir /opt/jackett
+sudo mv Jackett/* /opt/jackett
+sudo chown -R $UNAME:$UGROUP /opt/jackett
+
+sudo cp templates/jackett/jackett /etc/init.d/jackett
+sudo sed -i 's/RUN_AS=username/RUN_AS='$UNAME'/g' /etc/init.d/jackett
+sudo chmod +x /etc/init.d/jackett
+sudo update-rc.d jackett defaults
+sudo service jackett start
+
