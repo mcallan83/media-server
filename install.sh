3e280e5184827f9ffc2c96a7cfd0a52b05035c3a
Mike Callan <mcallan83@gmail.com>
2015-09-17 23:11:47 -0500

    sudo rm jackett

--- install.sh
+++ install.sh
@@ -127,8 +127,8 @@ sudo mkdir /opt/jackett
 sudo mv Jackett/* /opt/jackett
 sudo chown -R $UNAME: /opt/jackett
 
-rm Jackett.Mono.v0.6.4.tar.bz2
-rm -rf Jackett
+sudo rm Jackett.Mono.v0.6.4.tar.bz2
+sudo rm -rf Jackett
 
 sudo tee "/etc/init/jackett.conf" > /dev/null <<EOF
 description "Upstart Script: Jackett"
