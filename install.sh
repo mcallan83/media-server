7360848deda66cb87b19aed11f3462e032895218
Mike Callan <mcallan83@gmail.com>
2015-09-19 00:29:54 -0500

    wetty

--- install.sh
+++ install.sh
@@ -288,6 +288,18 @@ server {
     subs_filter 'url = a.href;' '';
     subs_filter 'return url' 'return "http://"+window.location.hostname+":9117"+url';
   }
+  #
+  location /wetty {
+      proxy_pass http://127.0.0.1:3000/wetty;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_read_timeout 43200000;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Host $http_host;
+      proxy_set_header X-NginX-Proxy true;
+  }
 }
 EOF
 
