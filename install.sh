5eb374a25c176762b89b274954eb3e8f61a713b7
Mike Callan <mcallan83@gmail.com>
2015-09-20 12:14:29 -0500

    enable everything

--- install.sh
+++ install.sh
@@ -50,14 +50,14 @@ sudo apt-get install xmlstarlet -y
 # Ajenti
 ################################################################################
 
-# wget http://repo.ajenti.org/debian/key -O- | sudo apt-key add -
-# echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" | sudo tee -a /etc/apt/sources.list
-# sudo apt-get update -y
-# sudo apt-get install ajenti -y
+wget http://repo.ajenti.org/debian/key -O- | sudo apt-key add -
+echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update -y
+sudo apt-get install ajenti -y
 
-# sudo sed -i 's/"enable": true/"enable": false/g' /etc/ajenti/config.json
+sudo sed -i 's/"enable": true/"enable": false/g' /etc/ajenti/config.json
 
-# sudo service ajenti restart
+sudo service ajenti restart
 
 
 ################################################################################
@@ -71,94 +71,94 @@ sudo service avahi-daemon restart
 # Couch Potato
 ################################################################################
 
-# cd /opt
-# sudo git clone https://github.com/RuudBurger/CouchPotatoServer.git couchpotato
-# cd $SCRIPTPATH
+cd /opt
+sudo git clone https://github.com/RuudBurger/CouchPotatoServer.git couchpotato
+cd $SCRIPTPATH
 
-# sudo chown -R $UNAME: /opt/couchpotato
-# sudo chmod -R 755 /opt/couchpotato
+sudo chown -R $UNAME: /opt/couchpotato
+sudo chmod -R 755 /opt/couchpotato
 
-# sudo tee "/etc/init/couchpotato.conf" > /dev/null <<EOF
-# description "Upstart Script: CouchPotato"
-# start on runlevel [2345]
-# stop on runlevel [016]
-# setuid $UNAME
-# setgid $UGROUP
-# exec python /opt/couchpotato/CouchPotato.py
-# EOF
-# sudo chmod +x /etc/init/couchpotato.conf
+sudo tee "/etc/init/couchpotato.conf" > /dev/null <<EOF
+description "Upstart Script: CouchPotato"
+start on runlevel [2345]
+stop on runlevel [016]
+setuid $UNAME
+setgid $UGROUP
+exec python /opt/couchpotato/CouchPotato.py
+EOF
+sudo chmod +x /etc/init/couchpotato.conf
 
-# sudo service couchpotato start
+sudo service couchpotato start
 
 
 ################################################################################
 # Deluge
 ################################################################################
 
-# sudo add-apt-repository -y ppa:deluge-team/ppa
-# sudo apt-get update -y
-# sudo apt-get install deluged deluge-webui -y
+sudo add-apt-repository -y ppa:deluge-team/ppa
+sudo apt-get update -y
+sudo apt-get install deluged deluge-webui -y
 
-# sudo tee "/etc/init/deluge.conf" > /dev/null <<EOF
-# description "Upstart Script: Deluge"
-# start on runlevel [2345]
-# stop on runlevel [016]
-# exec start-stop-daemon -S -c $UNAME:$UGROUP -k 022 -x /usr/bin/deluged -- -d
-# EOF
-# sudo chmod +x /etc/init/deluge.conf
+sudo tee "/etc/init/deluge.conf" > /dev/null <<EOF
+description "Upstart Script: Deluge"
+start on runlevel [2345]
+stop on runlevel [016]
+exec start-stop-daemon -S -c $UNAME:$UGROUP -k 022 -x /usr/bin/deluged -- -d
+EOF
+sudo chmod +x /etc/init/deluge.conf
 
-# sudo tee "/etc/init/deluge-web.conf" > /dev/null <<EOF
-# description "Upstart Script: Deluge Web"
-# start on started deluge
-# stop on stopping deluge
-# exec start-stop-daemon -S -c $UNAME:$UGROUP -k 027 -x /usr/bin/deluge-web
-# EOF
-# sudo chmod +x /etc/init/deluge-web.conf
+sudo tee "/etc/init/deluge-web.conf" > /dev/null <<EOF
+description "Upstart Script: Deluge Web"
+start on started deluge
+stop on stopping deluge
+exec start-stop-daemon -S -c $UNAME:$UGROUP -k 027 -x /usr/bin/deluge-web
+EOF
+sudo chmod +x /etc/init/deluge-web.conf
 
-# sudo service deluge start
-# sudo service deluge-web start
+sudo service deluge start
+sudo service deluge-web start
 
 ################################################################################
 # Jackett
 ################################################################################
 
-# sudo wget http://jackett.net/Download/v0.6.4/Jackett.Mono.v0.6.4.tar.bz2
-# sudo tar -xvf Jackett.Mono.v0.6.4.tar.bz2
-# sudo mkdir /opt/jackett
-# sudo mv Jackett/* /opt/jackett
-# sudo chown -R $UNAME: /opt/jackett
+sudo wget http://jackett.net/Download/v0.6.4/Jackett.Mono.v0.6.4.tar.bz2
+sudo tar -xvf Jackett.Mono.v0.6.4.tar.bz2
+sudo mkdir /opt/jackett
+sudo mv Jackett/* /opt/jackett
+sudo chown -R $UNAME: /opt/jackett
 
-# sudo rm Jackett.Mono.v0.6.4.tar.bz2
-# sudo rm -rf Jackett
+sudo rm Jackett.Mono.v0.6.4.tar.bz2
+sudo rm -rf Jackett
 
-# sudo tee "/etc/init/jackett.conf" > /dev/null <<EOF
-# description "Upstart Script: Jackett"
-# start on runlevel [2345]
-# stop on runlevel [016]
-# setuid $UNAME
-# setgid $UGROUP
-# exec mono /opt/jackett/JackettConsole.exe
-# EOF
-# sudo chmod +x /etc/init/jackett.conf
+sudo tee "/etc/init/jackett.conf" > /dev/null <<EOF
+description "Upstart Script: Jackett"
+start on runlevel [2345]
+stop on runlevel [016]
+setuid $UNAME
+setgid $UGROUP
+exec mono /opt/jackett/JackettConsole.exe
+EOF
+sudo chmod +x /etc/init/jackett.conf
 
-# sudo service jackett start
+sudo service jackett start
 
 
 ################################################################################
 # Plex
 ################################################################################
 
-# sudo curl http://shell.ninthgate.se/packages/shell-ninthgate-se-keyring.key | sudo apt-key add -
-# echo "deb http://www.deb-multimedia.org wheezy main non-free" | sudo tee -a /etc/apt/sources.list.d/deb-multimedia.list
-# echo "deb http://shell.ninthgate.se/packages/debian wheezy main" | sudo tee -a /etc/apt/sources.list.d/plex.list
-# sudo apt-get update -y
-# sudo apt-get install deb-multimedia-keyring -y --force-yes
-# sudo apt-get update -y
-# sudo apt-get install plexmediaserver -y
+sudo curl http://shell.ninthgate.se/packages/shell-ninthgate-se-keyring.key | sudo apt-key add -
+echo "deb http://www.deb-multimedia.org wheezy main non-free" | sudo tee -a /etc/apt/sources.list.d/deb-multimedia.list
+echo "deb http://shell.ninthgate.se/packages/debian wheezy main" | sudo tee -a /etc/apt/sources.list.d/plex.list
+sudo apt-get update -y
+sudo apt-get install deb-multimedia-keyring -y --force-yes
+sudo apt-get update -y
+sudo apt-get install plexmediaserver -y
 
-# sudo sed -i 's/PLEX_MEDIA_SERVER_USER=plex/PLEX_MEDIA_SERVER_USER='$UNAME'/g' /etc/default/plexmediaserver
+sudo sed -i 's/PLEX_MEDIA_SERVER_USER=plex/PLEX_MEDIA_SERVER_USER='$UNAME'/g' /etc/default/plexmediaserver
 
-# sudo service plexmediaserver restart
+sudo service plexmediaserver restart
 
 
 ################################################################################
@@ -312,7 +312,7 @@ server {
 EOF
 
 cd /etc/nginx/sites-enabled
-sudo ln -s /etc/nginx/sites-available/media media
+sudo ln -s /etc/nginx/sites-available/sites sites
 cd $SCRIPTPATH
 
 sudo htpasswd -b -c /etc/nginx/htpasswd $UNAME $UPASS
@@ -324,17 +324,17 @@ sudo service nginx restart
 # Wetty
 ################################################################################
 
-# curl -sL https://deb.nodesource.com/setup | sudo bash -
-# sudo apt-get install nodejs -y
-# sudo npm install wetty -g
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install nodejs -y
+sudo npm install wetty -g
 
-# sudo tee "/etc/init/wetty.conf" > /dev/null <<EOF
-# description "Upstart Script: Wetty"
-# start on started mountall
-# stop on shutdown
-# respawn
-# respawn limit 20 5
-# exec sudo -u root wetty -p 3000
-# EOF
+sudo tee "/etc/init/wetty.conf" > /dev/null <<EOF
+description "Upstart Script: Wetty"
+start on started mountall
+stop on shutdown
+respawn
+respawn limit 20 5
+exec sudo -u root wetty -p 3000
+EOF
 
-# sudo service wetty start
+sudo service wetty start
